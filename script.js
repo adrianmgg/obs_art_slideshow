05b6ff520c709e85e393926d94046f40dda8db9a
Adrian Guerra <2adrian2@gmail.com>
2020-08-19 14:22:23 -0700

    factor theme template out into other file

--- script.js
+++ script.js
@@ -120,9 +120,18 @@ document.addEventListener('mouseleave', function(e) {
 });
 
 (async function main(){
+	// load images list
 	imagesList = (await fetch('images.json').then(response=>response.json())).map(x=>new SlideshowEntry(x));
 	imagesListIndex = 0;
+	// load template
 	let templateContents = await fetch('theme/slideshow_template.html').then(response=>response.text());
 	template.innerHTML = templateContents;
+	// load theme stylesheet
+	let templateStylesheet = document.createElement('link');
+	document.head.appendChild(templateStylesheet);
+	templateStylesheet.rel = 'stylesheet';
+	templateStylesheet.href = './theme/slideshow_theme.css';
+	await nextEventFirePromise(templateStylesheet, 'load');
+	// start slideshow
 	nextImage();
 })();
