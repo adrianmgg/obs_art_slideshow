5692dff1a4ddaa0dd54e350587f3bff915ef9861
adrianmgg <2adrian2@gmail.com>
2021-09-30 14:47:12 -0700

    add port option to server.py

--- server.py
+++ server.py
@@ -1,7 +1,10 @@
-import runpy
-import sys
+import sys, argparse, runpy
 
-sys.argv.append('8932')
+parser = argparse.ArgumentParser()
+parser.add_argument('--port', type=int, default=8932, help='specify which port to use, or pass 0 to get an arbitrary unused port')
+args = parser.parse_args()
+
+sys.argv[1:] = [str(args.port)]
 
 if sys.version_info.major == 3:
     runpy.run_module('http.server', run_name='__main__')
